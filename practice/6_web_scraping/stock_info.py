c8568bb0427d9985da2d7da0939254482ddeec0f
broczniok <broczniok@griddynamics.com>
2024-05-30 15:30:13 +0200

    Same code possible solution in comment

--- practice/6_web_scraping/stock_info.py
+++ practice/6_web_scraping/stock_info.py
@@ -34,10 +34,28 @@ Links:
 
 # Profile link: https://finance.yahoo.com/quote/AAL/profile/ with AAL name of stock
 
+# To fix this code i need to add HEADER and unzip response
+
 import requests
 from bs4 import BeautifulSoup
 import re
+import gzip
 from datetime import datetime
+
+HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Cache-Control": "max-age=0",
+    }
+
 
 def sheet_creator(title: str, data: dict):
     # lengths of every column
@@ -91,7 +109,7 @@ def name_and_code_filler(data: dict, company: object, subsite: str):
     if '?' in url_suffix:
         url_suffix = url_suffix.split('?')[0]
     profile_url = "https://finance.yahoo.com" + url_suffix + subsite
-    comp_page = requests.get(profile_url)
+    comp_page = requests.get(profile_url, headers=HEADERS)
     comp_soup = BeautifulSoup(comp_page.content, "html.parser")
     return comp_soup
 
