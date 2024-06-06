ce3f742008d254e9bdc9fecf1f8a1d1a7e61d6e1
broczniok <broczniok@griddynamics.com>
2024-06-06 17:28:13 +0200

    concurrency done

--- practice/7_concurrency/task2_apod_api/template.py
+++ practice/7_concurrency/task2_apod_api/template.py
@@ -1,15 +1,40 @@
-API_KEY = "YOUR_KEY"
+import os
+import requests
+import concurrent.futures 
+from datetime import datetime, timedelta
+
+API_KEY = "APIKEY"
 APOD_ENDPOINT = 'https://api.nasa.gov/planetary/apod'
 OUTPUT_IMAGES = './output'
 
-
 def get_apod_metadata(start_date: str, end_date: str, api_key: str) -> list:
-    pass
+    url = f"{APOD_ENDPOINT}?api_key={api_key}&start_date={start_date}&end_date={end_date}"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
 
+def download_image(url: str, path: str):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(path, 'wb') as f:
+        f.write(response.content)
 
 def download_apod_images(metadata: list):
-    pass
+    if not os.path.exists(OUTPUT_IMAGES):
+        os.makedirs(OUTPUT_IMAGES)
 
+    with concurrent.futures.ThreadPoolExecutor() as ex:
+        futures = []
+        for item in metadata:
+            if item['media_type'] == 'image':
+                img_url = item['url']
+                img_date = item['date']
+                img_extension = os.path.splitext(img_url)[1]
+                img_path = os.path.join(OUTPUT_IMAGES, f"{img_date}{img_extension}")
+                futures.append(ex.submit(download_image, img_url, img_path))
+                
+        for future in futures:
+            future.result()
 
 def main():
     metadata = get_apod_metadata(
@@ -18,7 +43,6 @@ def main():
         api_key=API_KEY,
     )
     download_apod_images(metadata=metadata)
-
 
 if __name__ == '__main__':
     main()
