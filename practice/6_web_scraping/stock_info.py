ca6e5167f17e3ea34d41f70b3293e0ab1578d1d4
broczniok <broczniok@griddynamics.com>
2024-06-12 13:00:38 +0200

    Comments partial removal

--- practice/6_web_scraping/stock_info.py
+++ practice/6_web_scraping/stock_info.py
@@ -32,28 +32,13 @@ Links:
     - lxml docs: https://lxml.de/
 """
 
-
-from typing import Tuple
-import urllib.request
 import ssl
-import gzip
-from io import BytesIO
 from bs4 import BeautifulSoup
-from pprint import pprint  
 import requests
 from http import cookiejar
-from requests.cookies import cookiejar_from_dict, cookielib
-from requests.structures import CaseInsensitiveDict
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 import random
 
 ssl._create_default_https_context = ssl._create_stdlib_context
-
 
 user_agent = [
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36',
@@ -68,13 +53,14 @@ user_agent = [
     'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/77.0',
 ]
 
+
 class BlockAll(cookiejar.CookiePolicy):
     return_ok = set_ok = domain_return_ok = path_return_ok = lambda self, *args, **kwargs: False
     netscape = True
     rfc2965 = hide_cookie2 = False
 
 
-def make_request(url: str) -> tuple[int, str]:
+def make_request(url: str) -> tuple[int, bytes]:
     req = requests.get(url, headers={'User-Agent': random.choice(user_agent)}, timeout=5)
     if req.status_code == 200:
         return 200, req.content
@@ -86,11 +72,12 @@ def make_request(url: str) -> tuple[int, str]:
 def get_soup(url) -> BeautifulSoup:
     return BeautifulSoup(make_request(url)[1], 'html.parser')
 
+
 def get_codes():
     url_name_code = "https://finance.yahoo.com/most-active"
     soup = get_soup(url_name_code)
-    codes= {"Code": [], "Name": []}
-    
+    codes = {"Code": [], "Name": []}
+
     code_tds = soup.find_all("td", {"aria-label": "Symbol"})
     for td in code_tds:
         codes["Code"].append(td.text.strip())
@@ -101,9 +88,8 @@ def get_codes():
 
 
 def get_filtered_data_soup(company: str, code: str):
-    url_profile = "https://finance.yahoo.com/quote/"+str(company)+"/profile/"
+    url_profile = "https://finance.yahoo.com/quote/" + str(company) + "/profile/"
 
-    
     data = {"Name": [], "Code": [], "Country": [], "Employees": [], "CEO": [], "CEO Year Born": []}
     data["Code"].append(code)
     data["Name"].append(company)
@@ -113,27 +99,27 @@ def get_filtered_data_soup(company: str, code: str):
     if country_divs:
         last_country_div = country_divs[-1]
         data["Country"].append(last_country_div.text.strip())
-    dds = soup.find_all("dd") 
+    dds = soup.find_all("dd")
     for dd in dds:
         strong_tag = dd.find("strong")
         if strong_tag:
             data["Employees"].append(strong_tag.text)
-    
-    tables = soup.find_all("table", {"class": "svelte-mj92za"}) 
+
+    tables = soup.find_all("table", {"class": "svelte-mj92za"})
     for table in tables:
         tds = table.find_all("td")
         for i, td in enumerate(tds):
             if i % 5 == 0:
                 data["CEO"].append(td.text)
             elif (i - 4) % 5 == 0:
-                data["CEO Year Born"].append(td.text) 
-    
+                data["CEO Year Born"].append(td.text)
+
     return data
 
 
 def get_total_cash_52_range(company: str, code: str):
-    url_statistic = "https://finance.yahoo.com/quote/"+str(company)+"/key-statistics/"
-    
+    url_statistic = "https://finance.yahoo.com/quote/" + str(company) + "/key-statistics/"
+
     data = {"Name": [], "Code": [], "Total Cash": [], "52-week Change": []}
     data["Code"].append(code)
     data["Name"].append(company)
@@ -146,18 +132,18 @@ def get_total_cash_52_range(company: str, code: str):
 
         data["Total Cash"].append(total_cash_td.text.strip())
         data["52-week Change"].append(week_change_td.text.strip())
-    
+
     return data
 
 
 def get_blackrock(company: str, code: str):
-    url_holders = "https://finance.yahoo.com/quote/"+str(code)+"/holders/"
-    data = {"Name": [], "Code": [], "Shares": [], "Date Reported": [], "% Out": [], "Value":[]}
+    url_holders = "https://finance.yahoo.com/quote/" + str(code) + "/holders/"
+    data = {"Name": [], "Code": [], "Shares": [], "Date Reported": [], "% Out": [], "Value": []}
     data["Code"].append(code)
     data["Name"].append(company)
     soup = get_soup(url_holders)
     table_trs = soup.find_all("tr", {"class": "svelte-1s2g2l0"})
-    
+
     for tr in table_trs:
         tds = tr.find_all("td")
         if len(tds) >= 5 and tds[0].text.strip().lower() == 'blackrock inc.':
@@ -165,10 +151,8 @@ def get_blackrock(company: str, code: str):
             data["Date Reported"].append(tds[2].text.strip())
             data["% Out"].append(tds[3].text.strip())
             data["Value"].append(tds[4].text.strip())
-    
+
     return data
-    
-    
 
 
 '''
@@ -176,15 +160,17 @@ def get_blackrock(company: str, code: str):
     Sheet's fields: Name, Code, Country, Employees, CEO Name, CEO Year Born.
 
 '''
+
+
 def first_task():
     youngest = []
     codes = get_codes()["Code"]
     names = get_codes()["Name"]
     youngest_name = ''
-    for i in range(0, len(codes)): 
-        data = get_filtered_data_soup(codes[i], names[i]) 
-        current_name = names[i] 
-        current_code = codes[i] 
+    for i in range(0, len(codes)):
+        data = get_filtered_data_soup(codes[i], names[i])
+        current_name = names[i]
+        current_code = codes[i]
 
         current_country = data["Country"]
         current_employees = data["Employees"]
@@ -193,13 +179,12 @@ def first_task():
         current_youngest_CEO = 0
 
         for y in range(1, len(current_CEO_name)):
-            if current_CEO_year_born[y] != '-- ' and int(current_CEO_year_born[y]) >  current_youngest_CEO:
-                
+            if current_CEO_year_born[y] != '-- ' and int(current_CEO_year_born[y]) > current_youngest_CEO:
                 youngest_name = str(current_CEO_name[y])
                 current_youngest_CEO = int(current_CEO_year_born[y])
-        
-        youngest.append([current_youngest_CEO, youngest_name, current_name, current_code, current_country, current_employees])
 
+        youngest.append(
+            [current_youngest_CEO, youngest_name, current_name, current_code, current_country, current_employees])
 
     result_pretty_table = "==================================== 5 stocks with most youngest CEOs ===================================\n"
     result_pretty_table += "| Name        | Code | Country       | Employees | CEO Name                             | CEO Year Born |\n"
@@ -210,6 +195,7 @@ def first_task():
 
     print(result_pretty_table)
 
+
 #first_task()
 
 '''
@@ -217,25 +203,25 @@ def first_task():
     Sheet's fields: Name, Code, 52-Week Change, Total Cash
 '''
 
+
 def second_task():
     best_52 = []
     codes = get_codes()["Code"]
     names = get_codes()["Name"]
     best_52_week_change = 0
-    for i in range(0, len(codes)): 
-        data = get_total_cash_52_range(codes[i], names[i]) 
+    for i in range(0, len(codes)):
+        data = get_total_cash_52_range(codes[i], names[i])
         if not data["52-week Change"]:
             continue
         current_name = names[i]
-        current_code = codes[i] 
+        current_code = codes[i]
 
         current_total_cash = data["Total Cash"]
         current_52_week_change = float(data["52-week Change"][0][:-1])
-       
 
-        if float(current_52_week_change) >  best_52_week_change:
+        if float(current_52_week_change) > best_52_week_change:
             best_52_week_change = current_52_week_change
-        
+
         best_52.append([current_name, current_code, current_52_week_change, current_total_cash])
 
     result_pretty_table = "==================================== 10 stocks with best 52-Week Change ===================================\n"
@@ -247,8 +233,8 @@ def second_task():
 
     print(result_pretty_table)
 
-#second_task()
 
+second_task()
 
 '''
 3. 10 largest holds of Blackrock Inc. You can find related info on the Holders tab.
@@ -269,22 +255,22 @@ def third_task():
         current_name = names[i]
 
         current_code = codes[i]
-         
 
         current_shares = float(data["Shares"][0][:-1])
         current_date_reported = data["Date Reported"]
         current_perc_out = data["% Out"]
         current_value = data["Value"]
 
-        biggest_blackrock.append([current_name, current_code, current_shares, current_date_reported, current_perc_out, current_value])
-    
+        biggest_blackrock.append(
+            [current_name, current_code, current_shares, current_date_reported, current_perc_out, current_value])
+
     result_pretty_table = "==================================== 10 largest holds of Blackrock Inc ===================================\n"
     result_pretty_table += "| Name        | Code | Shares       | Date Reported | % Out     | Value      |\n"
     result_pretty_table += "===========================================================================================================\n"
     biggest_blackrock.sort(key=lambda x: x[2])
     for data in reversed(biggest_blackrock[-10:]):
         result_pretty_table += f"| {data[0]} | {data[1]} | {str(data[2])} | {data[3]} | {data[4]} | {data[5]} |\n"
-    
+
     print(result_pretty_table)
 
-third_task()
+#third_task()
