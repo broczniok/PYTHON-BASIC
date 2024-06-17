55d6e4768daab35b05a11bd226acafd726aac9a5
broczniok <broczniok@griddynamics.com>
2024-06-17 15:59:38 +0200

    Changed getting codes, first and third task

--- practice/6_web_scraping/stock_info.py
+++ practice/6_web_scraping/stock_info.py
@@ -67,25 +67,42 @@ def make_request(url: str) -> tuple[int, bytes]:
         return 200, req.content
     else:
         print("Make request doesnt work")
-        return None
+        return req.status_code, None
 
 
 def get_soup(url) -> BeautifulSoup:
     time.sleep(10)
-    return BeautifulSoup(make_request(url)[1], 'html.parser')
+    status_code, content = make_request(url)
+    if status_code != 200:
+        return None
+    return BeautifulSoup(content, 'html.parser')
 
 
 def get_codes():
-    url_name_code = "https://finance.yahoo.com/most-active?count=100&offset=0"
-    soup = get_soup(url_name_code)
     codes = {"Code": [], "Name": []}
+    offset = 0
+    count = 100
 
-    code_tds = soup.find_all("td", {"aria-label": "Symbol"})
-    for td in code_tds:
-        codes["Code"].append(td.text.strip())
-    name_tds = soup.find_all("td", {"aria-label": "Name"})
-    for td in name_tds:
-        codes["Name"].append(td.text.strip())
+    while True:
+        url = f"https://finance.yahoo.com/most-active?count={count}&offset={offset}"
+        soup = get_soup(url)
+
+        if soup is None:
+            break
+
+        code_tds = soup.find_all("td", {"aria-label": "Symbol"})
+        name_tds = soup.find_all("td", {"aria-label": "Name"})
+
+        if not code_tds or not name_tds:
+            break
+
+        for td in code_tds:
+            codes["Code"].append(td.text.strip())
+        for td in name_tds:
+            codes["Name"].append(td.text.strip())
+
+        offset += count
+
     return codes
 
 
@@ -97,6 +114,8 @@ def get_filtered_data_soup(company: str, code: str):
     data["Name"].append(company)
 
     soup = get_soup(url_profile)
+    if soup is None:
+        return None
     country_divs = soup.find_all("div", {"class": "address svelte-wxp4ja"})
     if country_divs:
         last_country_div = country_divs[-1]
@@ -110,11 +129,15 @@ def get_filtered_data_soup(company: str, code: str):
     tables = soup.find_all("table", {"class": "svelte-mj92za"})
     for table in tables:
         tds = table.find_all("td")
-        for i, td in enumerate(tds):
-            if i % 5 == 0:
-                data["CEO"].append(td.text)
-            elif (i - 4) % 5 == 0:
-                data["CEO Year Born"].append(td.text)
+        ceo_name_index = None
+    for i, td in enumerate(tds):
+        text = td.text.strip().lower()
+        if 'ceo' in text:
+            ceo_name_index = i - 1
+            data["CEO"].append(tds[ceo_name_index].text.strip())
+        elif ceo_name_index is not None and i == ceo_name_index + 4:
+            data["CEO Year Born"].append(td.text.strip())
+            ceo_name_index = None
 
     return data
 
@@ -125,8 +148,10 @@ def get_total_cash_52_range(company: str, code: str):
     data = {"Name": [], "Code": [], "Total Cash": [], "52-week Change": []}
     data["Code"].append(code)
     data["Name"].append(company)
-
     soup = get_soup(url_statistic)
+    if soup is None:
+        return None
+
     total_cash_tds = soup.find_all("td", {"class": "value svelte-vaowmx"})
     if total_cash_tds:
         total_cash_td = total_cash_tds[14]
@@ -138,17 +163,19 @@ def get_total_cash_52_range(company: str, code: str):
     return data
 
 
-def get_blackrock(company: str, code: str):
-    url_holders = "https://finance.yahoo.com/quote/" + str(code) + "/holders/"
-    data = {"Name": [], "Code": [], "Shares": [], "Date Reported": [], "% Out": [], "Value": []}
-    data["Code"].append(code)
-    data["Name"].append(company)
+def get_blackrock():
+    url_holders = "https://finance.yahoo.com/quote/BLK/holders/"
+    data = {"Name": [], "Shares": [], "Date Reported": [], "% Out": [], "Value": []}
+    if get_soup(url_holders) is None:
+        return None
     soup = get_soup(url_holders)
     table_trs = soup.find_all("tr", {"class": "svelte-1s2g2l0"})
 
     for tr in table_trs:
         tds = tr.find_all("td")
-        if len(tds) >= 5 and tds[0].text.strip().lower() == 'blackrock inc.':
+        if len(tds) >= 5:
+            data["Name"].append(tds[0].text.strip())
+            print("Name: ", tds[0].text.strip())
             data["Shares"].append(tds[1].text.strip())
             data["Date Reported"].append(tds[2].text.strip())
             data["% Out"].append(tds[3].text.strip())
@@ -163,6 +190,8 @@ def first_task(codes, names):
     youngest_name = ''
     for i in range(0, len(codes)):
         data = get_filtered_data_soup(codes[i], names[i])
+        if data is None:
+            continue
         current_name = names[i]
         current_code = codes[i]
         current_country = data["Country"][0].split(',')[1]
@@ -202,24 +231,7 @@ def first_task(codes, names):
 
     youngest.sort(key=lambda x: x[5])
 
-    max_lengths = [len(h) for h in header]
-
-    for row in youngest:
-        for i, item in enumerate(row):
-            max_lengths[i] = max(max_lengths[i], len(str(item)))
-
-    row_format = "| " + " | ".join([f"{{:<{max_length}}}" for max_length in max_lengths]) + " |"
-
-    table_width = sum(max_lengths) + len(max_lengths) * 3 + 1
-    title = "5 stocks with the youngest CEOs"
-    result_pretty_table = f"{'=' * ((table_width - len(title) - 2) // 2)} {title} {'=' * ((table_width - len(title) - 2) // 2)}\n"
-    result_pretty_table += row_format.format(*header) + "\n"
-    result_pretty_table += "=" * table_width + "\n"
-
-    for data in reversed(youngest[-5:]):
-        result_pretty_table += row_format.format(*map(str, data)) + "\n"
-
-    print(result_pretty_table)
+    print_pretty_table(youngest, 5, header, "5 stocks with most youngest CEOs")
 
 
 def second_task(codes, names):
@@ -227,6 +239,8 @@ def second_task(codes, names):
     best_52_week_change = 0
     for i in range(0, len(codes)):
         data = get_total_cash_52_range(codes[i], names[i])
+        if data is None:
+            continue
         if not data["52-week Change"]:
             continue
         current_name = names[i]
@@ -244,75 +258,59 @@ def second_task(codes, names):
 
     header = ["Name", "Code", "52-week Change", "Total Cash"]
 
+    print_pretty_table(best_52, 10, header, "10 stocks with best 52-Week Change")
+
+
+def print_pretty_table(table, rows, header, title):
     max_lengths = [len(h) for h in header]
 
-    for row in best_52:
+    for row in table:
         for i, item in enumerate(row):
             max_lengths[i] = max(max_lengths[i], len(str(item)))
 
     row_format = "| " + " | ".join([f"{{:<{max_length}}}" for max_length in max_lengths]) + " |"
 
     table_width = sum(max_lengths) + len(max_lengths) * 3 + 1
-    title = "10 stocks with the best 52-week change"
     result_pretty_table = f"{'=' * ((table_width - len(title) - 2) // 2)} {title} {'=' * ((table_width - len(title) - 2) // 2)}\n"
     result_pretty_table += row_format.format(*header) + "\n"
     result_pretty_table += "=" * table_width + "\n"
 
-    for data in reversed(best_52[-10:]):
+    for data in reversed(table[-rows:]):
         result_pretty_table += row_format.format(*data) + "\n"
 
     print(result_pretty_table)
 
 
-def third_task(codes, names):
+def third_task():
     biggest_blackrock = []
 
-    for i in range(0, len(codes)):
-        data = get_blackrock(names[i], codes[i])
+    for i in range(0, 11):
+        data = get_blackrock()
+        if data is None:
+            continue
         if not data["Shares"]:
             continue
-        current_name = names[i]
-
-        current_code = codes[i]
-
-        current_shares = float(data["Shares"][0][:-1])
-        current_date_reported = data["Date Reported"][0]
-        current_perc_out = data["% Out"][0]
-        current_value = data["Value"][0]
+        current_name = data["Name"][i]
+        current_shares = float(data["Shares"][i][:-1])
+        current_date_reported = data["Date Reported"][i]
+        current_perc_out = data["% Out"][i]
+        current_value = data["Value"][i]
 
         biggest_blackrock.append(
-            [current_name, current_code, current_shares, current_date_reported, current_perc_out, current_value])
+            [current_name, current_shares, current_date_reported, current_perc_out, current_value])
 
-    header = ["Name", "Code", "Shares", "Date Reported", "% Out", "Value"]
+    header = ["Name", "Shares", "Date Reported", "% Out", "Value"]
 
-    biggest_blackrock.sort(key=lambda x: x[2])
-    max_lengths = [len(h) for h in header]
+    biggest_blackrock.sort(key=lambda x: x[1])
 
-    for row in biggest_blackrock:
-        for i, item in enumerate(row):
-            max_lengths[i] = max(max_lengths[i], len(str(item)))
-
-    row_format = "| " + " | ".join([f"{{:<{max_length}}}" for max_length in max_lengths]) + " |"
-
-    table_width = sum(max_lengths) + len(max_lengths) * 3 + 1
-    title = "10 largest holds of Blackrock Inc"
-    result_pretty_table = f"{'=' * ((table_width - len(title) - 2) // 2)} {title} {'=' * ((table_width - len(title) - 2) // 2)}\n"
-    result_pretty_table += row_format.format(*header) + "\n"
-    result_pretty_table += "=" * table_width + "\n"
-
-    for data in reversed(biggest_blackrock[-10:]):
-        result_pretty_table += row_format.format(*data) + "\n"
-
-    print(result_pretty_table)
+    print_pretty_table(biggest_blackrock, 10, header, "10 largest holds of Blackrock Inc.")
 
 
-get_codes_var = get_codes()
+if __name__ == "__main__":
+    get_codes_var = get_codes()
+    codes = get_codes_var["Code"]
+    names = get_codes_var["Name"]
 
-codes = get_codes_var["Code"]
-names = get_codes_var["Name"]
-
-first_task(codes, names)
-
-second_task(codes, names)
-
-third_task(codes, names)
+    first_task(codes, names)
+    second_task(codes, names)
+    third_task()
